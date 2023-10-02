d53691d1ab237c461dfe9b7b6c66ddeca63bfcf8
XRequiem1 <144215937+XRequiem1@users.noreply.github.com>
2023-10-02 13:51:50 +0300

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,51 @@
+#14
+#x = int(input('1 число:  '))
+#print(x, x + 1, x + 2, sep='\n')
+
+#16
+#a = int(input('длина ребра:    '))
+#v = a ** 3
+#S = 6 * a ** 2
+#print(f'обьем = {v}')
+#print('площадь полной поверхности =', S)
+
+#20
+#a_1 = int(input('1 член прогрессии'))
+#d = int(input('d '))
+#n = int(input('n '))
+#print(a_1 + d * (n-1))
+
+#26
+#n = int(input())
+#print('   _~_    ' * n)
+#print('  (o o)   ' * n)
+#print(' /  v  \  ' * n)
+#print('/(  _  )\ ' * n)
+#print('  ^^ ^^   ' * n)
+
+
+#n = int(input())
+#print(n // 2 * 2 + 2)
+# 2 -> 4
+# 3 -> 4
+
+#18
+#x = int(input())
+#print('Следующее за числом', x, "число:", x + 1)
+#print('Для числа ', x, "предыдущее число:",x - 1)
+
+#32
+#h = int(input())
+#a = int(input())
+#b = int(input())
+#print((h - a) // (a - b) + 1)
+
+#15
+#h = int(input())
+#a = int(input())
+#b = int(input())
+#print(h + a + b)
+
+#21
+#n = int(input())
+#print(n, n * 2, n * 3, n * 4, n * 5, sep = '---')
